072facae09bd7fd27d4f6a3198c95c0108842469
Alina Ray <a.alina.ray@gmail.com>
2019-07-31 14:51:48 +0300

    Updates

---
+++ precache-manifest.89f2db07fe3bc56cfb6c00dd8ef27aa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d7d7dda5878c8e7a13400c194a1460",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "8d5ac71e751aabe8fbab",
+    "url": "/tabs-with-router/static/css/main.02f31337.chunk.css"
+  },
+  {
+    "revision": "5789c5a7c7dafdabb00a",
+    "url": "/tabs-with-router/static/js/2.1b1c988e.chunk.js"
+  },
+  {
+    "revision": "8d5ac71e751aabe8fbab",
+    "url": "/tabs-with-router/static/js/main.34cb9163.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
